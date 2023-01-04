5805ab35cdb65198a14e394ed6c793c97a201cd7
jungsangwon <top1tiger@naver.com>
2023-01-05 00:09:36 +0900

    정률 할인 정책 추가

--- src/main/java/springPractice/core/order/OrderServiceImpl.java
+++ src/main/java/springPractice/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package springPractice.core.order;
 
 import springPractice.core.discount.DiscountPolicy;
 import springPractice.core.discount.FixDiscountPolicy;
+import springPractice.core.discount.RateDiscountPolicy;
 import springPractice.core.member.Member;
 import springPractice.core.member.MemberRepository;
 import springPractice.core.member.MemoryMemberRepository;
@@ -9,7 +10,8 @@ import springPractice.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
---
+++ src/test/java/springPractice/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,35 @@
+package springPractice.core.discount;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import springPractice.core.member.Grade;
+import springPractice.core.member.Member;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다")
+    void vip_check(){
+        //given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        Assertions.assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("VIP가 아니면 할인이 적용되지 않아야 한다")
+    void basic_check(){
+        //given
+        Member member = new Member(1L, "memberBASIC", Grade.BASIC);
+        //when
+        int discount = discountPolicy.discount(member, 10000);
+        //then
+        Assertions.assertThat(discount).isEqualTo(0);
+    }
+}
