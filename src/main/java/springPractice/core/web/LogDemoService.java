b84e29860dc87b2fb9befbec2e5d49480ec5da50
jungsangwon <top1tiger@naver.com>
2023-02-03 01:02:09 +0900

    request scope

---
+++ src/main/java/springPractice/core/web/LogDemoService.java
@@ -0,0 +1,18 @@
+package springPractice.core.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Service;
+import springPractice.core.common.MyLogger;
+
+@Service
+@RequiredArgsConstructor
+public class LogDemoService {
+
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
+    public void logic(String id) {
+//        MyLogger myLogger = myLoggerProvider.getObject();
+        myLogger.log("service id = "+id);
+    }
+}
